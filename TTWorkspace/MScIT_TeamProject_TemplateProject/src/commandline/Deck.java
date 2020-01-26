05f77c2dd6f9332216e241321c575e6829aafcea
Fiona Johnson <9905602j@student.gla.ac.uk>
2020-01-26 18:05:57 +0000

    2nd commit, is now shuffleing the deck, makeing players and dealing cards to them

--- TTWorkspace/MScIT_TeamProject_TemplateProject/src/commandline/Deck.java
+++ TTWorkspace/MScIT_TeamProject_TemplateProject/src/commandline/Deck.java
@@ -2,19 +2,18 @@ package commandline;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Deck {
 	
 	private Card[]cards;
-	private String[] categories;
 	
 	private int nextCardPos;
 	
-	public Deck(int sizeOfDeck, String[] categories) {
+	public Deck(int sizeOfDeck) {
 		nextCardPos = 0;
 		cards = new Card[sizeOfDeck];
-		this.categories = categories;
 	}
 	
 	public void addNewCard(Card card) {
@@ -22,10 +21,32 @@ public class Deck {
 		nextCardPos++;
 	}
 	
-	public void testPrint() {
-		for(int i=0;i<categories.length;i++) {
-			System.out.println(categories[i]);;
+	public void shuffle() {
+		Card[]shuffledDeck = new Card[cards.length];
+		int i = 0;
+		while(i<cards.length) {
+			Random card = new Random();
+			int randomIndex = card.nextInt(cards.length);
+			if(cards[randomIndex]==null) {
+			}else {
+				shuffledDeck[i] = cards[randomIndex];
+				cards[randomIndex]=null;
+				i++;
+			}
 		}
+		cards = shuffledDeck;
+	}
+	
+	public void dealCards(Player[] players) {
+
+		for(int i = 0;i<cards.length;i = i+4) {
+			for(int j = 0;j<players.length;j++) {
+				players[j].getHand().addNewCard(cards[i+j]);
+			}
+		}
+	}
+	
+	public void testPrint() {
 		for(int i=0;i<cards.length;i++) {
 			System.out.println(cards[i].toString());
 		}
