5ed81aa1378aec983d1a4bfa9ea7a755225bcb48
Fiona Johnson <9905602j@student.gla.ac.uk>
2020-02-04 15:39:07 +0000

    Added some limited and ugly online functionality, commented and neatened up all the code so far.

--- TTWorkspace/MScIT_TeamProject_TemplateProject/src/game/HumanPlayer.java
+++ TTWorkspace/MScIT_TeamProject_TemplateProject/src/game/HumanPlayer.java
@@ -3,22 +3,27 @@ package game;
 import java.util.Scanner;
 
 public class HumanPlayer extends Player {
-	private int playerID;
-	private int sizeOfHand;
-	private Deck playersHand;
+//	private int playerID;
+//	private int sizeOfHand;
+//	private Deck playersHand;
 	
 	public HumanPlayer(int sizeOfHand, int ID) {
 		super(sizeOfHand, ID);
 	}
 	
+//used by commandLine mode to allow the user to pick the category when they are the active player
+//category picked returned as an int representing that categories place in the cards categories array
 	public int pickCategory(int numberOfCats, String[]cats ) {
+//display the options to the player
 		System.out.println("It is your turn to select a category, the categories are:");
 		for(int i = 0; i<numberOfCats;i++) {
 			System.out.println("\t" + (i+1) + ":\t" + cats[i]);
 		}
+//get the players choice
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the number for your attribute:");
 		int catPicked = scanner.nextInt()-1;
+		scanner.close();
 		return catPicked;
 	}
 
